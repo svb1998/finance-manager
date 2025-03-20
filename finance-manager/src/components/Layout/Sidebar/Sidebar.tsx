18e0bcd12e76821b81616982e63f95bf50da01ed
Steven Valencia <1998.svb@gmail.com>
2025-03-20 19:04:52 +0100

    WIP: Implementing sidebar items. Next -> Sync with current url

--- finance-manager/src/components/Layout/Sidebar/Sidebar.tsx
+++ finance-manager/src/components/Layout/Sidebar/Sidebar.tsx
@@ -1,7 +1,9 @@
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import './Sidebar.css'
 import { useEffect, useState } from 'react';
-import { LayoutDashboard } from 'lucide-react';
+import { ArrowLeftRight, Bolt, LayoutDashboard } from 'lucide-react';
+import { setActivePage } from '../../../redux/states';
+import Item from './components/Item/Item';
 
 interface Props {
     className?: string;
@@ -10,22 +12,35 @@ interface Props {
 
 export default function Sidebar({className, id}: Props) {
 
+    const listIndex = {
+        dashboard: "dashboard",
+        transactions: "transactions",
+        categories: "categories", 
+        settings: "settings",
+    }
+
+    const dispatch = useDispatch();
+
     const sidebarStatus = useSelector(store => store.sidebar)
+
+    const activePage = sidebarStatus.activePage
 
     const [sidebarClassname, setsidebarClassname] = useState<string>(sidebarStatus.isOpen ? 'sidebar-expanded' : 'sidebar-collapsed');
 
     useEffect(() => {
-  
         setsidebarClassname(sidebarStatus.isOpen ? 'sidebar-expanded' : 'sidebar-collapsed');
     }, [sidebarStatus]);
 
     return (
         <aside className={`${className}  sidebar-container ${sidebarClassname}`} id={`${id}`}>
 
-            <ul className='sidebar-list'>
-                <li className='sidebar-item'>
-                    <LayoutDashboard className='glow-icon' strokeWidth={2} stroke='greenyellow' size={24} />
-                </li>
+            <ul className='sidebar-list items-group'>
+                <Item pageTitle="Dashboard" isActive={activePage == listIndex.dashboard} onClick={() => dispatch(setActivePage(listIndex.dashboard))} icon={LayoutDashboard} />
+                <Item pageTitle="Transactions" isActive={activePage == listIndex.transactions} onClick={() => dispatch(setActivePage(listIndex.transactions))} icon={ArrowLeftRight}/>
+                
+            </ul>
+            <ul className='sidebar-list '>
+                <Item pageTitle='Settings' isActive={activePage == listIndex.settings} onClick={() => dispatch(setActivePage(listIndex.settings))} icon={Bolt}/>
             </ul>
             
             
